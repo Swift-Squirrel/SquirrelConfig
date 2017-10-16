0e20d9783eff7e1ad3dfcefab8fdf612afc5fe32
Filip Klembara <filip@klembara.pro>
2017-10-16 16:29:08 +0200

    Observing for updates

--- Sources/SquirrelConfig/SquirrelConfig.swift
+++ Sources/SquirrelConfig/SquirrelConfig.swift
@@ -12,12 +12,23 @@ import Yams
 public class SquirrelConfig {
 
     /// Shared config
-    public static var shared: SquirrelConfig? = nil
+    public static var shared: SquirrelConfig? = nil {
+        didSet {
+            guard let instance = shared else {
+                return
+            }
+            observers.forEach { observer in
+                observer(instance.rootNode)
+            }
+        }
+    }
 
     /// default config name
     public static let defaultConfigName = ".squirrel.yml"
 
     private let yaml: Node
+
+    private static var observers = [(rootNode: Node) -> ()]()
 
     /// Constructs shared config and set `SquirrelConfig.shared` to this instance
     ///
@@ -33,6 +44,13 @@ public class SquirrelConfig {
         }
         yaml = rootNode
         SquirrelConfig.shared = self
+    }
+
+    /// Set closure to update values when config file changes
+    ///
+    /// - Parameter closure: Updating closure
+    public static func addObserver(closure: @escaping (Node) -> ()) {
+        observers.append(closure)
     }
 
     /// Get root node
